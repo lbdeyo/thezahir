cddbe5614c2e33ee109bf7f549df0dd2d32ef5b9
lbdeyo <lbdeyo@gmail.com>
2025-11-25 16:16:41 -0600

    Adding a footer and stuff

---
+++ app/components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer className="w-full py-2">
+      <div className="max-w-5xl mx-auto px-4 sm:px-8">
+        <div className="px-4 sm:px-12">
+          <p className="text-[#ada173] font-['Baskerville'] text-base text-left">
+            © 2025 The Zahir Productions, Inc. Original theater, film, and
+            storytelling.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
