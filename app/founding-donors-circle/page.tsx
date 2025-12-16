cbd81402c573fe061b7d636919b0d15a3614d158
lbdeyo <lbdeyo@gmail.com>
2025-12-15 19:31:56 -0600

    Fixing font for Leonard and Jeanie

--- app/founding-donors-circle/page.tsx
+++ app/founding-donors-circle/page.tsx
@@ -305,7 +305,9 @@ function FoundingDonorsCircleContent() {
                   >
                     <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
                   </svg>
-                  <span>Leonard Hough and Jeanie Ockenden</span>
+                  <span className="font-['Baskerville']">
+                    Leonard Hough and Jeanie Ockenden
+                  </span>
                 </div>
                 <div className="flex items-center text-2xl text-[#ada173] font-medium font-['Baskerville']">
                   <svg
@@ -381,5 +383,3 @@ export default function FoundingDonorsCircle() {
     </Suspense>
   );
 }
-
-
