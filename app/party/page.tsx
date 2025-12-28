a72c61d2091e80120f9171f799f750f1d6f65809
lbdeyo <lbdeyo@gmail.com>
2025-12-28 10:27:31 -0600

    Adding the program to the party page

--- app/party/page.tsx
+++ app/party/page.tsx
@@ -66,43 +66,25 @@ export default function Party() {
         {/* Main Content */}
         <main className="w-full px-4 sm:px-8 pt-0 pb-12 relative">
           <div
-            className="px-4 sm:px-12 pt-4 pb-6 relative"
+            className="px-4 sm:px-12 pt-4 pb-3 relative"
             style={{
               background: "rgba(173, 161, 115, 0.90)",
             }}
             id="gradient-start"
           >
-            <h1
-              className="text-4xl sm:text-5xl md:text-[3.75rem] text-black mb-2 pt-3 font-['thirsty-rough-two']"
-              style={{ fontWeight: 800, fontStyle: "normal" }}
-            >
-              Join us
-            </h1>
-            <p className="text-2xl sm:text-4xl pt-3 text-black mb-4 sm:mb-6 font-medium font-['Baskerville']">
+            <h1 className="text-3xl sm:text-4xl pt-3 text-black sm:mb-4 font-medium font-['Baskerville']">
               <strong>
-                {" "}
-                <em>For</em> The Zahir&apos;s Inaugural Gala Benefit
+                <em>Join us</em> for The Zahir&apos;s Inaugural Gala Benefit
               </strong>
-            </p>
+            </h1>
           </div>
           <div
             className="px-4 sm:px-12 py-6"
-            style={{ background: "rgba(0,0,0,0.8)" }}
+            style={{ background: "rgba(0,0,0,0.9)" }}
           >
             <h2 className="text-2xl sm:text-3xl font-semibold text-[#ada173] mb-6 font-['Baskerville']">
               Tuesday, January 6, 2026 • Hyde Park Theatre • Austin, TX
             </h2>
-            <p className="text-2xl text-[#ada173] mb-6 font-medium font-['Baskerville']">
-              Step into an evening of elegant delights, where the clink of
-              champagne glasses mingles with lively conversation and laughter.
-              Enjoy handcrafted refreshments and delectable bites as you mingle
-              with friends and creators in the warm, inviting atmosphere of Hyde
-              Park Theatre. The night will feature live music and special
-              performances, offering a taste of the creative spirit behind The
-              Zahir. You&apos;ll be part of an inspiring community dedicated to
-              meaningful storytelling and dialogue. Raise a glass, savor the
-              ambiance, and celebrate the launch of The Zahir in style.
-            </p>
             <div className="flex justify-center mb-8">
               <a
                 href="https://www.eventbrite.com/e/the-zahirs-inaugural-benefit-gala-tickets-1977771996281?aff=oddtdtcreator&fbclid=IwY2xjawOsYR5leHRuA2FlbQIxMABicmlkETFPWklFSFNKVWlydkJ6T3lTc3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHklYHM9esJABBZEkqD08nmUmSMfkIkknacNxEaD78VbMutNONCoi4VBt1H6d_aem_fFznBueuXb9eV7M9eGwvZQ"
@@ -113,6 +95,93 @@ export default function Party() {
                 Get Tickets
               </a>
             </div>
+            <p className="text-2xl text-[#ada173] mb-6 font-medium font-['Baskerville']">
+              Step into an evening of elegant delights, where the clink of
+              champagne glasses mingles with lively conversation and laughter.
+              Enjoy handcrafted refreshments and delectable bites as you mingle
+              with friends and creators in the warm, inviting atmosphere of Hyde
+              Park Theatre.
+            </p>
+            <h2 className="text-2xl sm:text-3xl font-semibold text-[#ada173] mb-6 font-['Baskerville']">
+              The Program
+            </h2>
+            <ul className="list-disc pl-6 text-[#ada173] text-xl sm:text-2xl font-['Baskerville'] space-y-4 mb-8">
+              <li className="flex items-center space-x-3">
+                <em>7:00 PM</em> - Reception
+              </li>
+              <li className="flex items-center space-x-3">
+                <em>7:30-9:00 PM</em> - Entertainment
+              </li>
+              <li className="flex items-center space-x-3">
+                <Image
+                  src="/img/company/lauraderamo-headshot.jpg"
+                  alt="Laura D'Eramo"
+                  width={75}
+                  height={75}
+                  className="w-[75px] h-[75px] aspect-square object-cover rounded-full border-2 border-[#ada173] shadow shrink-0"
+                />
+                <span>
+                  &quot;Antony&apos;s Funeral Oration&quot; by Laura
+                  D&apos;Eramo
+                </span>
+              </li>
+              <li className="flex items-center space-x-3">
+                <Image
+                  src="/img/company/chad-portrait.jpg"
+                  alt="Chad Nichols"
+                  width={75}
+                  height={75}
+                  className="w-[75px] h-[75px] aspect-square object-cover rounded-full border-2 border-[#ada173] shadow shrink-0"
+                />
+                <span>Live music by Chad Nichols</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                <Image
+                  src="/img/company/hope-irish.jpg"
+                  alt="Hope Irish"
+                  width={75}
+                  height={75}
+                  className="w-[75px] h-[75px] aspect-square object-cover rounded-full border-2 border-[#ada173] shadow shrink-0"
+                />
+                <span>Live music by Hope Irish</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                <Image
+                  src="/img/company/RachelWestHeadshot.jpg"
+                  alt="Rachel West"
+                  width={75}
+                  height={75}
+                  className="w-[75px] h-[75px] aspect-square object-cover rounded-full border-2 border-[#ada173] shadow shrink-0"
+                />
+                <span>Live music by Rachel West</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                <Image
+                  src="/img/company/jessica-cohen-portrait.jpg"
+                  alt="Jessica Cohen"
+                  width={75}
+                  height={75}
+                  className="w-[75px] h-[75px] aspect-square object-cover rounded-full border-2 border-[#ada173] shadow shrink-0"
+                />
+                <span>
+                  An <em>Apprehension</em> monologue, and live music, by Jessica
+                  Cohen
+                </span>
+              </li>
+              <li className="flex items-center space-x-3">
+                <Image
+                  src="/img/company/lb-portrait.jpg"
+                  alt="L.B. Deyo"
+                  width={75}
+                  height={75}
+                  className="w-[75px] h-[75px] aspect-square object-cover rounded-full border-2 border-[#ada173] shadow shrink-0"
+                />
+                <span>A speech and moderated discussion by L.B. Deyo</span>
+              </li>
+              <li className="flex items-center space-x-3">
+                <em>9:00 PM</em> - Cocktails &amp; conversation
+              </li>
+            </ul>
             <Image
               src="/img/party1/champagne-coin.jpg"
               alt="Party"
