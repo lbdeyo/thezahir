969458224467d60ef138636c54649d9293c5894f
lbdeyo <lbdeyo@gmail.com>
2026-01-28 09:53:05 -0600

    Updating URL

--- app/components/GiveGabButton.tsx
+++ app/components/GiveGabButton.tsx
@@ -1,7 +1,7 @@
 export default function GiveGabButton() {
   return (
     <a
-      href="https://dog.givegab.com/p2p/484802/benjamin-deyo?dog_name=austin2026"
+      href="https://www.amplifyatx.org/p2p/484802/benjamin-deyo"
       target="_blank"
       rel="noopener noreferrer"
       className="inline-block bg-black text-[#ada173] font-bold px-6 py-3 rounded-lg text-lg sm:text-xl text-center font-['Baskerville'] border-2 border-transparent hover:bg-[#ada173] hover:text-black hover:border-black transition-colors shadow-lg"
